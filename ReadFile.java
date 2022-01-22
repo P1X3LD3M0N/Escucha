56724787922f755664489b73c40df07aead59469
P1X3LD3M0N <77390209+P1X3LD3M0N@users.noreply.github.com>
2022-01-22 10:59:50 +0530

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,7 +23,7 @@ public class ReadFile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent i = getIntent();
-        FILE_NAME = "example.txt";
+        FILE_NAME = i.getStringExtra("filePass");
         setContentView(R.layout.activity_read_file);
         result = (TextView) findViewById(R.id.textView5);
         FileInputStream fis = null;
@@ -39,6 +41,7 @@ public class ReadFile extends AppCompatActivity {
             result.setText(sb.toString());
 
         } catch (FileNotFoundException e) {
+            Toast.makeText(getApplicationContext(),"No History yet",Toast.LENGTH_LONG).show();
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
@@ -53,6 +56,10 @@ public class ReadFile extends AppCompatActivity {
         }
     }
 }
+
+
+
+
 
 
 
